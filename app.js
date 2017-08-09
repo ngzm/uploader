7e8a497ee6da8ffa032a1b9ee71a1dbcee629b2f
naoki-nagazumi <naoki.nagazumi@ntt-at.co.jp>
2017-08-09 17:45:10 +0900

    正式リリースに向けてログモードなどサーバモジュールを微修正

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/upmain', express.static(path.join(__dirname, 'public')));
 
 // Application Main Routing
 app.use('/upload', uploader);
